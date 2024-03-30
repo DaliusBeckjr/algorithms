07d51785b70d3fd30922420c6a4b9a7a7415207c
Dalius Beck <daliusbeckxiv@gmail.com>
2024-03-29 21:01:30 -0700

    update sandbox

---
+++ Fundamentals/python/notes/basics.md
@@ -0,0 +1,41 @@
+snake case
+l_name = ''
+f_name = ''
+
+using the ('_') this is a form of snake case that is used a lot in python
+
+String Manipulation
+using f'' (f string) we can add all the values into a single print() or return 
+ex. print(f'my name is {f_name} {l_name}')
+
+string.upper() -> returns a copy of the string with all the char in uppercase
+string.lower() -> returns a copy of the string with all characters in lowercase
+string.count(substring) -> returns a list of values where string is split at the given characer
+
+string.find(substring) -> returns the index of the start of the first occurance of substring with string
+
+string.Isalnum() -> returns boolean depending depending on whether the string length is > 0 and all characters are alphanumeric(). strings that include spaces and punctuation will return false for this method. similar method includes .isalpha(), .isdigit(), islower(), isupper(), and so on.
+
+string.join(list) -> returns a string that is all string within our set concatenated
+
+string.endswith() -> returns a boolean based upon whether the last characters of string match substring
+
+List Manipulation
+
+an array in python is called a list
+some_list = [1,2,3,4,5]
+
+.append() -> adds the element to the end of the list
+.pop() -> removes and returns the last element from the list 
+.reverse() -> reverses the order of elements in the list
+.remove() -> searches list for the value that you want to remove
+dont have a .silce()..we have [::] ->
+syntax: sequence[start:stop:step]
+
+len()
+
+
+loops:
+range() ->
+syntax: range(start:stop:step)
+ex: for i in range(0,5,1):
--- Fundamentals/python/sandbox.py
+++ Fundamentals/python/sandbox.py
@@ -62,22 +62,92 @@ def str_to_arr(str):
 
 # square_sum([1,5,6])
 def square_sum(numbers):
-    # if not isinstance(n, list):
-    #     print("Input is not a list")
-    #     return
+    if not isinstance(n, list):
+        print("Input is not a list")
+        return
     
-    # arr = n
-    # total_sum = 0
+    arr = n
+    total_sum = 0
 
-    # for num in arr:
-    #     squared_num = num ** 2
-    #     total_sum += squared_num
+    for num in arr:
+        squared_num = num ** 2
+        total_sum += squared_num
     
-    # print(total_sum)
-    sum = 0
-    for n in numbers:
-        sum = sum + n*n
-        print(sum)
-    return sum
+    print(total_sum)
+    # sum = 0
+    # for n in numbers:
+    #     sum = sum + n*n
+    #     print(sum)
+    # return sum
 
-square_sum([1, 5, 6])
+# square_sum([1, 5, 6])
+
+some_list = [1,2,3,4,5]
+
+# for i in range():
+#     print(some_list[i])
+
+is_instructor = False
+
+# if is_instructor:
+#     print("yessirrr")
+# else:
+#     print("no sirrrski")
+
+def fizz_buzz():
+    for i in range(1, 101):
+        
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# print(fizz_buzz())
+
+# def starterkit(n: int):
+#     print(f"I am only {n}")
+
+# starterkit(5)
+
+def make_negative(number: int):
+    print(number)
+    return number if number <= 0 else number * -1
+
+# other answer
+    # if number <= 0:
+    #     print(number)
+    #     return number
+    # else:
+    #     number = number * -1
+    #     print(number)
+    #     return number
+
+# make_negative(5)
+# make_negative(0)
+# make_negative(-4)
+
+def one_to_two_fifty_five():
+    # print 1 - 255
+    for i in range(1, 256, 1):
+        print(i)
+
+# print(one_to_two_fifty_five())
+
+def odd_num():
+    for i in range(1,255,2):
+        print(i)
+
+# print(odd_num())
+
+def sum_of_nums():
+    total_sum = 0  # Initialize the sum variable to 0
+    for i in range(1, 256, 1):  # Iterate from 1 to 255 (inclusive)
+        total_sum += i  # Add the current value of 'i' to the sum
+    print(total_sum)  # Print the final sum
+
+# Call the function to calculate and print the sum
+sum_of_nums()
